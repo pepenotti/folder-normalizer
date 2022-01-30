d04df127d95219c1877e80502dce04a66cfc317f
pepenotti <41025937+pepenotti@users.noreply.github.com>
2022-01-30 00:10:39 -0300

    Master (#1)
    * Add .gitignore and .gitattributes.
    
    * Add project files.
    
    Co-authored-by: Antonio Franchinotti <afranchi89@hotmail.com>

---
+++ folder-normalizer.console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace folder_normalizer.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Origin path:");
+            var originPath = Console.ReadLine();
+
+            Console.WriteLine("Origin extension:");
+            var originExtension = Console.ReadLine();
+
+            Console.WriteLine("Path to normalize:");
+            var pathToNormalize = Console.ReadLine();
+
+            Console.WriteLine("Extension of files to normalize:");
+            var extensionToNormalize = Console.ReadLine();
+
+            var fns = new FolderNormalizerService();
+            fns.Normalize(originPath, pathToNormalize, originExtension, extensionToNormalize);
+
+            Console.WriteLine("Completed");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ folder-normalizer.core/FolderNormalizerService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace folder_normalizer.core
+{
+    public class FolderNormalizerService : IFolderNormalizerService
+    {
+       
+        private const char FORDWARD_DASH = '/';
+        private const char BACKWARD_DASH = '\\';
+
+        public void Normalize(string originFolderPath, string pathToNormalize, string originExtension, string extensionToNormalize)
+        {
+            var originFiles = Directory.GetFiles(originFolderPath, "*."+originExtension);
+            var filesToNormalize = Directory.GetFiles(pathToNormalize, "*." + extensionToNormalize);
+
+            if (pathToNormalize.Last() != FORDWARD_DASH || pathToNormalize.Last() != BACKWARD_DASH)
+                pathToNormalize += pathToNormalize.Contains(FORDWARD_DASH) ? FORDWARD_DASH : BACKWARD_DASH;
+
+            Console.WriteLine($"Origin ({originFiles.Length}) | Destination ({filesToNormalize.Length})");
+
+            foreach(var ftn in filesToNormalize)
+            {
+                var ftnFileName = Path.GetFileNameWithoutExtension(ftn);
+                var originFilePath = originFiles.FirstOrDefault(of => of.Contains(ftnFileName));
+
+                if (String.IsNullOrWhiteSpace(originFilePath))
+                {
+                    Console.WriteLine($"Deleted {pathToNormalize + ftn}");
+                    File.Delete(ftn);
+                }
+            };
+        }
+    }
+}
